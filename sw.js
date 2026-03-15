7b04a0d2ea7b6f0943c86ea91eca8c0463efc6fb
evcar <89557079+evcarga@users.noreply.github.com>
2026-03-14 21:29:44 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'fajardo-pwa-v1';
+
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'sw.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache abierto ✅');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
